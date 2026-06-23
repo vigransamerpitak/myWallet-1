7f3b0f66336474ec5653a8cab3199ee3fbad0322
it1kbi <vigran.samerpitak@gmail.com>
2026-06-23 10:03:29 +0700

    23062026.0001

--- config.js
+++
@@ -1,4 +0,0 @@
-// config.js - ไฟล์เก็บค่าเชื่อมต่อฐานข้อมูล Supabase
-const SUPABASE_URL = "https://tseftsnzyrbcrajearxz.supabase.co";
-const SUPABASE_KEY = "sb_publishable_0cfkLjDHdpoDicbNNf68OA_ZWsZdPak"; // 👈 ตัวนี้คือ Anon Key ตัวเดิมของคุณโบ๊ท
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
